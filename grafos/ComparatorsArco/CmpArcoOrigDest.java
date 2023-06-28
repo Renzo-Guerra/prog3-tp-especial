689c6a9dca01a7b0a4789609015e0489d6880e91
renzo-guerra <renzoguerralatt@gmail.com>
2023-06-28 00:14:48 -0300

    ADD: correcciones de identacion

--- grafos/ComparatorsArco/CmpArcoOrigDest.java
+++ grafos/ComparatorsArco/CmpArcoOrigDest.java
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 package grafos.ComparatorsArco;
 
 import java.util.Comparator;
 
 import grafos.Arco;
 
-=======
-package grafos;
-
-import java.util.Comparator;
-
->>>>>>> 424892c51a796fcf57c9aa82e0ac3041270a9198
 public class CmpArcoOrigDest implements Comparator<Arco>{
 
   @Override
